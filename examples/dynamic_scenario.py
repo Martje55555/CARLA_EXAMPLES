e41051952e446117a67030619835f0a04a891532
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-01-21 13:07:20 -0600

    Update dynamic_scenario.py
    added pedestrians, and manipulated the weather conditions

--- examples/dynamic_scenario.py
+++ examples/dynamic_scenario.py
@@ -1,4 +1,3 @@
-from ast import Lambda
 import glob
 from multiprocessing.context import set_spawning_popen
 import os
@@ -18,6 +17,7 @@ import random
 import time
 import numpy as np
 import cv2
+import logging
 
 IM_WIDTH = 640
 IM_HEIGHT = 480
@@ -36,6 +36,9 @@ def process_img(image):
 def main():
     # list to store all actors to destroy together after simulation
     actor_list = []
+    #vehicles_list = []
+    walkers_list = []
+    all_id = []
 
     try:
         # client will be accepting requests from localhost/2000
@@ -57,9 +60,20 @@ def main():
         #world.apply_settings(settings)
 
         weather = carla.WeatherParameters(
-            cloudiness=80.0,
-            precipitation=100.0,
-            sun_altitude_angle=110.0)
+            cloudiness=50.0, 
+			precipitation=100.0,
+			precipitation_deposits=80.0,
+			wind_intensity=100.0,
+			sun_azimuth_angle=40.0,
+			sun_altitude_angle=40.0,
+			fog_density=10.0,
+			fog_distance=50.0,
+			wetness=50.0,
+			fog_falloff=0.0,
+			scattering_intensity=40.0, 
+			mie_scattering_scale=10.0,
+			rayleigh_scattering_scale=0.0331
+        )
 
         #or use precomputed weathers
         #weather = carla.WeatherParameters.WetCloudySunset
@@ -81,10 +95,65 @@ def main():
         # set vehicle to autopilot
         vehicle.set_autopilot(True)
 
-         # add more vehicles to the world
+        # add pedestrians to the world
+        world = client.get_world()
+        blueprintsWalkers = world.get_blueprint_library().filter("walker.pedestrian.*")
+        walker_bp = random.choice(blueprintsWalkers)
+
+        spawn_points = []
+        for i in range(80):
+            spawn_point = carla.Transform()
+            spawn_point.location = world.get_random_location_from_navigation()
+            if (spawn_point.location != None):
+                spawn_points.append(spawn_point)
+
+        batch = []
+        for spawn_point in spawn_points:
+            walker_bp = random.choice(blueprintsWalkers)
+            batch.append(carla.command.SpawnActor(walker_bp, spawn_point))
+
+        # apply the batch
+        results = client.apply_batch_sync(batch, True)
+        for i in range(len(results)):
+            if results[i].error:
+                logging.error(results[i].error)
+            else:
+                walkers_list.append({"id": results[i].actor_id})
+
+
+        batch = []
+        walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+        for i in range(len(walkers_list)):
+            batch.append(carla.command.SpawnActor(walker_controller_bp, carla.Transform(), walkers_list[i]["id"]))
+
+        # apply the batch
+        results = client.apply_batch_sync(batch, True)
+        for i in range(len(results)):
+            if results[i].error:
+                logging.error(results[i].error)
+            else:
+                walkers_list[i]["con"] = results[i].actor_id
+
+
+        for i in range(len(walkers_list)):
+            all_id.append(walkers_list[i]["con"])
+            all_id.append(walkers_list[i]["id"])
+        all_actors = world.get_actors(all_id)
+
+        world.wait_for_tick()
+
+        for i in range(0, len(all_actors), 2):
+            # start walker
+            all_actors[i].start()
+            # set walk to random point
+            all_actors[i].go_to_location(world.get_random_location_from_navigation())
+            # random max speed
+            all_actors[i].set_max_speed(1 + random.random())
+
+        # add more vehicles to the world
         transform.location += carla.Location(x=40, y=-3.2)
         transform.rotation.yaw = -180.0
-        for _ in range(0, 30):
+        for _ in range(0, 80):
             transform.location.x += 8.0
 
             bp = random.choice(blueprint_library.filter('vehicle'))
@@ -115,10 +184,14 @@ def main():
 
     finally:
         print('destroying actors')
-        #sensor.destroy()
+        sensor.destroy()
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
-        for actor in actor_list:
-            actor.destroy()
+        # for actor in actor_list:
+        #     actor.destroy()
+
+        for i in range(0, len(all_id), 2):
+            all_actors[i].stop()
+            client.apply_batch([carla.command.DestroyActor(x) for x in all_id])
         print('done.')
 
 if __name__ == '__main__':
